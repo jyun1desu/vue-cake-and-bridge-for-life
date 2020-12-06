c18d752cb665a2c7146845682a185a3dcd9f8809
jyun1 <jyuny12345@gmail.com>
2020-12-06 22:02:18 +0800

    shuffle function

--- src/modules/card.js
+++ src/modules/card.js
@@ -1,6 +1,7 @@
-function buildNewDeck(deck=[]){
+function buildNewDeck(){
     const suit = ['Spades','heart','diamond','club'];
     const number = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+    const deck =[]
     for(i=0;i<suit.length;i++){
         let card={};
         for(j=0;j<number.length;j++){
@@ -14,5 +15,11 @@ function buildNewDeck(deck=[]){
     return deck
 }
 
-const deck = buildNewDeck();
-console.log(deck)
+function shuffle(deck){
+    let i = deck.length;
+    while(i>1){
+        let index = Math.floor(Math.random() * i--);
+        [ deck[i] , deck[index] ] = [ deck[index] , deck[i] ]
+    }
+    return deck
+}
