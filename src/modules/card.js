35756679773fe8bf516aa4e989cf71c7543dc7d1
jyun1 <jyuny12345@gmail.com>
2020-12-06 23:10:06 +0800

    發牌功能

--- src/modules/card.js
+++ src/modules/card.js
@@ -1,25 +1,40 @@
-function buildNewDeck(){
-    const suit = ['Spades','heart','diamond','club'];
-    const number = [1,2,3,4,5,6,7,8,9,10,11,12,13];
-    const deck =[]
-    for(i=0;i<suit.length;i++){
-        let card={};
-        for(j=0;j<number.length;j++){
+//建立新牌組
+function buildNewDeck() {
+    const suit = ['Spades', 'heart', 'diamond', 'club'];
+    const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    const deck = []
+    for (i = 0; i < suit.length; i++) {
+        let card = {};
+        for (j = 0; j < number.length; j++) {
             card = {
-                suit:suit[i],
-                number:number[j]
+                suit: suit[i],
+                number: number[j]
             }
             deck.push(card)
         }
     }
     return deck
 }
-
-function shuffle(deck){
+//洗牌
+function shuffle(deck) {
     let i = deck.length;
-    while(i>1){
+    while (i > 1) {
         let index = Math.floor(Math.random() * i--);
-        [ deck[i] , deck[index] ] = [ deck[index] , deck[i] ]
+        [deck[i], deck[index]] = [deck[index], deck[i]]
     }
     return deck
 }
+//發牌(給四個玩家)
+function dealCards(playerAmount,deck) {
+    const players = [];
+    for (let i = 0; i < playerAmount; i++) {
+        const eachPlayer = [];
+        players.push(eachPlayer)
+        for (let i = 0;i<deck.length;i=i+4){
+            eachPlayer.push(deck[i])
+        }
+    }
+    return players
+}
+//
+console.log(dealCards(4,shuffle(buildNewDeck())))
