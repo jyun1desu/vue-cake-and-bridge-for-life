cff4e3ebde47e296bbedd3c7d6ab4b43f362b032
jyun1 <jyuny12345@gmail.com>
2021-01-07 14:21:36 +0800

    pad rwd add

--- src/components/Home/Logo.vue
+++ src/components/Home/Logo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="logo">
-    <img class="logo__image" src="@/assets/logo.svg" alt="">
+    <img class="logo__image" src="@/assets/logo.svg" alt="" />
     <img src=@/assets/name.jpg alt="" class="logo__name">
     <!-- <h1 class="game_name">糕橋一生</h1> -->
   </div>
@@ -8,36 +8,40 @@
 
 <script>
 export default {
-  name: 'Logo',
-}
+  name: "Logo",
+};
 </script>
 
 <style scoped lang="scss">
-.logo{
+.logo {
   margin: 30px auto 0;
   border-radius: 100%;
   background-color: #fff;
   text-align: center;
-  width:80vw;
+  width: 80vw;
   height: 80vw;
   padding: 30px;
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
   align-items: center;
-  &__image{
+  @include RWD($pad) {
+    width: 40vw;
+    height: 40vw;
+  }
+  &__image {
     width: 60%;
     margin-bottom: 20px;
   }
-  &__name{
+  &__name {
     width: 70%;
   }
 }
-    .game_name{
-      margin: 10px 0;
-      color:$title_font_color;
-      font-size: 28px;
-      font-weight: normal;
-      letter-spacing: 5px;
-    }
+.game_name {
+  margin: 10px 0;
+  color: $title_font_color;
+  font-size: 28px;
+  font-weight: normal;
+  letter-spacing: 5px;
+}
 </style>
