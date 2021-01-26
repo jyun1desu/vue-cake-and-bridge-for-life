46881609054137a02051ebba2af685ef045dea48
jyun1 <jyuny12345@gmail.com>
2021-01-26 11:55:34 +0800

    設置環境變數

--- src/db.js
+++ src/db.js
@@ -2,14 +2,14 @@ import firebase from "firebase/app";
 import "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCPvpuWyETLq0ag8WGlTddWUJjVc0kwP0Y",
-    authDomain: "cake-bridge-for-life.firebaseapp.com",
-    databaseURL: "https://cake-bridge-for-life-default-rtdb.firebaseio.com",
-    projectId: "cake-bridge-for-life",
-    storageBucket: "cake-bridge-for-life.appspot.com",
-    messagingSenderId: "787966748364",
-    appId: "1:787966748364:web:61513888a922b33deb4469",
-    measurementId: "G-V1RGW705P4"
+    apiKey: process.env.VUE_APP_APIKEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_DATABASE_URL,
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_APP_ID,
+    measurementId: process.env.VUE_APP_MEASUREMENT_ID
 };
 
 const db = firebase.initializeApp(firebaseConfig);
---
+++ src/dist/db.dev.js
@@ -0,0 +1,28 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _app = _interopRequireDefault(require("firebase/app"));
+
+require("firebase/database");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var firebaseConfig = {
+  apiKey: process.env.VUE_APP_APIKEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
+};
+
+var db = _app["default"].initializeApp(firebaseConfig);
+
+var _default = db;
+exports["default"] = _default;
